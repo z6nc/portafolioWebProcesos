37c9b328c38fb34454f7f695066fce9df1495650
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-08 18:33:26 -0500

    fix: add missing optimizeDeps configuration for react-youtube exclusion

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+   optimizeDeps: {
+    exclude: ["react-youtube"],
+  },
 })
