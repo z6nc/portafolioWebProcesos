727892e4c0231381de3fcc08b483ad7c2fa8b8c2
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-08 16:42:20 -0500

    Update project URLs in ListaProyectos for consistency

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -6,13 +6,14 @@ export const ListaProyectos = [
         datoImportante: "Alrededor de +40 usuarios  ",
         tecnologias: ["Next js", "TypeScript", "TalwindCss"],
         Año: "2025",
-        url: "https://andresvelasquezdev.vercel.app/proyectos/rimayninchik"
+        url: "/proyecto/MindStack"
     },
     {
         nombreProyecto: "Automatizacion de Registro de compras para un AutoLavado ",
         informacion: "Transformé un proceso de ventas manual en un sistema automatizado e inteligente para la toma de decisiones.",
         tecnologias: ["Bizagi Model", "SharePoint", "Power Automate", "Power Bi"],
         Año: "2025",
-        url: "https://andresvelasquezdev.vercel.app/proyectos/rimayninchik"
+        url: "/proyecto/Conewash"
+
     },
 ]
