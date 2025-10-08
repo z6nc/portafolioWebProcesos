e2e293728d020e874032024db412558007357e8a
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-08 16:42:41 -0500

    Add typography plugin to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography'
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  plugins: [typography],
+
 }
